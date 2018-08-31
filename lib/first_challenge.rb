1ef3fa585dcc718ec0d13897b0256db0f414690c
Nasser Sanou <nassersanou23@gmail.com>
2018-08-31 03:32:27 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
 
- contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_at("strawberry") 
+ contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_at(0) 
  contacts
   #remember to return your newly altered contacts hash!
   contacts
