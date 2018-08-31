2a33009e876049554a05fc548f9b455874b0e2a5
Nasser Sanou <nassersanou23@gmail.com>
2018-08-31 03:33:37 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
 
- contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_at(0) 
+# contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_at(0) 
  #OR 
  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry") 
  contacts
